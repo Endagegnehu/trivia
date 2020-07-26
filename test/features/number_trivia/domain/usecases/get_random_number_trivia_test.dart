e2628b65066bbca7f7cd522e05c14cc30baf9269
Endagegnehu <endukokam@gmail.com>
2020-07-27 00:11:43 +0300

    random number trivia test

--- test/features/number_trivia/domain/usecases/get_random_number_trivia_test.dart
+++ test/features/number_trivia/domain/usecases/get_random_number_trivia_test.dart
@@ -1,6 +1,7 @@
 import 'package:dartz/dartz.dart';
 import 'package:flutter_test/flutter_test.dart';
 import 'package:mockito/mockito.dart';
+import 'package:trivia/core/usecase/usecase.dart';
 import 'package:trivia/features/number_trivia/domain/entities/number_trivia.dart';
 import 'package:trivia/features/number_trivia/domain/repositories/number_trivia_repositories.dart';
 import 'package:trivia/features/number_trivia/domain/usecases/get_random_number_trivia.dart';
